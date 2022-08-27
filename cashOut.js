850e51bfb6f270944e3ba999468449711f53594a
John Reid <john.reid@elcom.cz>
2022-08-27 07:28:50 +0200

    Cash out all hashes, cash out all investments

--- cashOut.js
+++ cashOut.js
@@ -1,6 +1,8 @@
+import {checkTix, divestAll} from "chaseAlpha.js"
+
 /** @param {import(".").NS} ns */
 export async function main(ns) {
-	while (ns.hacknet.numHashes() > 50) {
-		ns.hacknet.spendHashes("Sell for Money");
-	}
+	const moolah = Math.floor(ns.hacknet.numHashes() / 4);
+	ns.hacknet.spendHashes("Sell for Money", "", moolah);
+	if (checkTix(ns, false)) divestAll(ns);
 }
